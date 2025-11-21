b903456c532dd61c8b9d87423fa468172d698e6b
Gabriel Gonzalez <gabriel.gonzalez@nolatech.ai>
2025-11-21 09:27:39 -0400

    chage urls

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://barber-app-backend-kj6s.onrender.com';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://bartop-p.up.railway.app/api';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
